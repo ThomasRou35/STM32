184b853b56e9d9d4770e63032b45773b42b02275
Rouleau Thomas <th.rouleau44@gmail.com>
2026-01-28 22:26:37 +0100

    stm32_M1

---
+++ STM32_M1/programmes/fonc_asm.h
@@ -0,0 +1,14 @@
+/*
+ * fonc_asm.h
+ *
+ *  Created on: Dec 19, 2024
+ *      Author: reboux
+ */
+
+#ifndef INC_FONC_ASM_H_
+#define INC_FONC_ASM_H_
+
+extern void allume(void);
+extern void eteint(void);
+
+#endif /* INC_FONC_ASM_H_ */
---
+++ STM32_M1/programmes/fonc_asm.s
@@ -0,0 +1,23 @@
+
+  .syntax unified
+
+  .text
+  .global allume
+  .global eteint
+  .thumb_func
+
+  .equ  GPIOB_BSRR,  0x40020418 // GPIOB Bit set reset register
+  .equ  GPIOB_ODR,  0x40020414 // GPIOB Output Data register
+
+  allume:
+  
+
+
+
+
+
+
+
+  eteint:
+
+   
